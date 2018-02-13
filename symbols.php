1b7362f48d0929af3a8cc47f276a6c222637e115
Andrew Kamal <kamalandrew55@gmail.com>
2018-02-13 16:39:58 -0500

    Create symbols.php

---
+++ symbols.php
@@ -0,0 +1,14 @@
+<?php
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://api.kucoin.com/v1/order?symbol=BTC&symbols=ETH&symbols=NEO
+");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+curl_setopt($ch, CURLOPT_POST, TRUE);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+var_dump($response);
