473956e57a2ecc374bb9d93a0ff4426d04da0098
Andrew Kamal <kamalandrew55@gmail.com>
2018-02-15 20:20:55 -0500

    Create dealorders.php

---
+++ dealorders.php
@@ -0,0 +1,12 @@
+<?php
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://api.kucoin.com/v1/deal-orders
+");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+var_dump($response);
